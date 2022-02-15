48b92896fca9c4b86b08489b93a8b2b7000d561f
ibroid <maulana858@gmail.com>
2022-02-15 14:46:48 +0700

    fiksasi notifikasiAkta.js

--- tasks/NotifikasiAkta.js
+++ tasks/NotifikasiAkta.js
@@ -7,69 +7,71 @@ const prisma = new PrismaClient()
 const moment = require('moment');
 const now = moment().locale('id').format('YYYY-MM-DD');
 
-async function debug() {
-    const data = await prisma.perkara_akta_cerai.findMany({
-        select: {
-            nomor_akta_cerai: true,
-            perkara_id: true,
-            tgl_akta_cerai: true,
-            perkara: {
-                select: {
-                    nomor_perkara: true,
-                    perkara_pihak1: {
-                        select: {
-                            nama: true,
-                            pihak: {
-                                select: {
-                                    telepon: true
+module.exports = {
+    start: async () => {
+        const data = await prisma.perkara_akta_cerai.findMany({
+            select: {
+                nomor_akta_cerai: true,
+                perkara_id: true,
+                tgl_akta_cerai: true,
+                perkara: {
+                    select: {
+                        nomor_perkara: true,
+                        perkara_pihak1: {
+                            select: {
+                                nama: true,
+                                pihak: {
+                                    select: {
+                                        telepon: true
+                                    }
                                 }
                             }
                         }
-                    }
-                },
-            }
-        },
-        where: {
-            tgl_akta_cerai: new Date(now)
-        }
-    })
-
-    const registerAkta = register_pemberitahuan.find(
-        (Element) => Element.keperluan == "pemberitahuan_akta"
-    );
-
-    if (data) {
-        data.forEach(row => {
-            console.log(row.nomor_perkara)
-            const telepon = row.perkara.perkara_pihak1[0].pihak.telepon;
-
-            const textBalasan = registerAkta.pesan;
-
-            if (telepon) {
-                console.log(`Kirim pesan ke ${telepon} dengan pesan ${textBalasan}`);
-                try {
-                    client.sendMessage(numberFormatter(telepon), textBalasan)
-                        .then(res => {
-
-                            console.log(`Notifikasi Terkirim ke ${telepon} pada pukul ${moment().format()}`);
-
-                            const logger = new Logger('host', `Pemberitahuan Jadwal Sidang Pertama kepada pihak dengan nomor ${telepon}`, 'notifikasi')
-
-                            logger.start()
-
-                        })
-                } catch (error) {
-                    await client
-                        .sendMessage(numberFormatter(String(process.env.DEVELOPER_CONTACT)), "Terdapat error \n\n" + error)
-                        .then((res) => res)
-                        .catch((err) => console.log(err));
-
+                    },
                 }
-
+            },
+            where: {
+                tgl_akta_cerai: new Date(now)
             }
         })
+
+        const registerAkta = register_pemberitahuan.find(
+            (Element) => Element.keperluan == "pemberitahuan_akta"
+        );
+
+        if (data) {
+
+            data.forEach(async row => {
+                console.log(row.nomor_perkara)
+                const telepon = row.perkara.perkara_pihak1[0].pihak.telepon;
+
+                const textBalasan = registerAkta.pesan;
+
+                if (telepon) {
+                    try {
+                        await client.sendMessage(numberFormatter(telepon), textBalasan)
+                            .then(res => {
+
+                                console.log(`Notifikasi Terkirim ke ${telepon} pada pukul ${moment().format()}`);
+
+                                const logger = new Logger('host', `Pemberitahuan Jadwal Sidang Pertama kepada pihak dengan nomor ${telepon}`, 'notifikasi')
+
+                                logger.start()
+
+                            })
+                    } catch (error) {
+                        await client
+                            .sendMessage(numberFormatter(String(process.env.DEVELOPER_CONTACT)), "Terdapat error \n\n" + error)
+                            .then((res) => res)
+                            .catch((err) => console.log(err));
+
+                    }
+
+                }
+            })
+        }
+
     }
 }
 
 
-debug();
